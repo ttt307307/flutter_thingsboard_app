5f108e25e8c6b83b4ed927967351801aff9f3129
谭德卫 <151540@winnermedical.com>
2023-07-13 14:25:59 +0800

    fix: 修改后端配置

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,5 @@
 abstract class ThingsboardAppConstants {
-  static final thingsBoardApiEndpoint = 'http://localhost:8080';
+  static final thingsBoardApiEndpoint = 'http://106.12.118.231:1088';
   static final thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
 
   /// Not for production (only for debugging)
